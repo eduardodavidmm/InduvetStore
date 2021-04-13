7ba12b5722de05790fca3765c690505a0514d64d
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2021-04-13 16:47:29 -0600

    Auth Modeled

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc Fetch all Users
@@ -15,7 +16,7 @@ const authUser = asyncHandler( async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
@@ -23,4 +24,11 @@ const authUser = asyncHandler( async (req, res) => {
     }
 })
 
-export { authUser }
+// @desc Get User Profile
+// @route GET /api/users/login
+// @access Public
+const getUserProfile = asyncHandler( async (req, res) => {
+    res.send('Success')
+})
+
+export { authUser, getUserProfile }
