dd0ed6a6e14fac90b423e1ef1b7f42903db3f204
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2021-06-07 21:01:53 -0600

    Delivered Set

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,118 +1,166 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import { getOrderDetails } from '../actions/orderActions'
-
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { getOrderDetails, deliverOrder } from '../actions/orderActions';
+import { ORDER_DELIVER_RESET } from '../constants/orderConstants';
 
 const OrderScreen = ({ match }) => {
-    const orderId = match.params.id
+  const orderId = match.params.id;
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
 
-    const orderDetails = useSelector((state) => state.orderDetails)
-    const { order, loading, error } = orderDetails
-
-    if (!loading) {
-    order.itemsPrice = order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-    }
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
 
-    useEffect(() => {
-        dispatch(getOrderDetails(orderId))
-    }, [])
+  if (!loading) {
+    order.itemsPrice = order.orderItems.reduce(
+      (acc, item) => acc + item.price * item.qty,
+      0
+    );
+  }
 
-    return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <>
-        <h1 className="mtopone">Orden {order._id}</h1>
-        <Row>
-                <Col className='mtopone' md={8}>
-                    <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h2>Envío</h2>
-                        <strong>Cliente: </strong>{order.user.name}
-                        <div>
-                            <strong>Direccion: </strong>
-                            {order.shippingAddress.address},{' '} {order.shippingAddress.city}
-                            <br/>
-                            <strong>Telefono: </strong>
-                            {order.shippingAddress.phone}
-                        </div>
-                        <div>
-                        {order.isDelivered ? <Message variant='success'>Entregado {order.deliveredAt}</Message> : <Message variant='danger'>Entrega pendiente</Message>}
-                        </div>
-                    </ListGroup.Item>
-                    
-                    <ListGroup.Item>
-                        <h2>Método de Pago</h2>
-                        <div>
-                            <strong>Paga con: </strong>
-                            {order.paymentMethod}
-                        </div>
-                    </ListGroup.Item>
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
 
-                    <ListGroup.Item>
-                        <h2>Articulos</h2>
-                        <div>
-                            <strong>Articulos de la orden: </strong>
-                            {order.orderItems.length === 0 ? <Message>No tiene articulos en su orden</Message> : 
-                            ( <ListGroup variant='flush'> 
-                            {order.orderItems.map((item, index) => (
-                                <ListGroup.Item keu={index}>
-                                    <Row>
-                                        <Col md={1}>
-                                            <Image src={item.image} alt={item.name} fluid rounded />
-                                        </Col>
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, []);
 
-                                        <Col>
-                                            <Link to={`/product/${item.name}`}>
-                                                {item.name}
-                                            </Link>
-                                        </Col>
+  const deliverHandler = () => {
+    dispatch(deliverOrder(order));
+  };
 
-                                        <Col md={4}>
-                                            {item.qty} x Lps. {item.price} = Lps. {item.qty * item.price}
-                                        </Col>
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <>
+      <h1 className='mtopone'>Orden {order._id}</h1>
+      <Row>
+        <Col className='mtopone' md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Envío</h2>
+              <strong>Cliente: </strong>
+              {order.user.name}
+              <div>
+                <strong>Direccion: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city}
+                <br />
+                <strong>Telefono: </strong>
+                {order.shippingAddress.phone}
+              </div>
+              <div>
+                {order.isDelivered ? (
+                  <Message variant='success'>
+                    Entregado {order.deliveredAt}
+                  </Message>
+                ) : (
+                  <Message variant='danger'>Entrega pendiente</Message>
+                )}
+              </div>
+            </ListGroup.Item>
 
-                                    </Row>
-                                </ListGroup.Item>
-                            ))}
-                            </ListGroup> ) }
-                        </div>
-                    </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col className='mtopone' md={4}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <h2>Resumen de la Orden</h2>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Articulos</Col>
-                                    <Col>Lps. {order.itemsPrice}</Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Envío</Col>
-                                    <Col>Lps. {order.shippingPrice}</Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Total</Col>
-                                    <Col>Lps. {order.totalPrice}</Col>
-                                </Row>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+            <ListGroup.Item>
+              <h2>Método de Pago</h2>
+              <div>
+                <strong>Paga con: </strong>
+                {order.paymentMethod}
+              </div>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Articulos</h2>
+              <div>
+                <strong>Articulos de la orden: </strong>
+                {order.orderItems.length === 0 ? (
+                  <Message>No tiene articulos en su orden</Message>
+                ) : (
+                  <ListGroup variant='flush'>
+                    {order.orderItems.map((item, index) => (
+                      <ListGroup.Item keu={index}>
+                        <Row>
+                          <Col md={1}>
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              fluid
+                              rounded
+                            />
+                          </Col>
+
+                          <Col>
+                            <Link to={`/product/${item.name}`}>
+                              {item.name}
+                            </Link>
+                          </Col>
+
+                          <Col md={4}>
+                            {item.qty} x Lps. {item.price} = Lps.{' '}
+                            {item.qty * item.price}
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                )}
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col className='mtopone' md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>Resumen de la Orden</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Articulos</Col>
+                  <Col>Lps. {order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Envío</Col>
+                  <Col>Lps. {order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>Lps. {order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              {loadingDeliver && <Loader />}
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type='button'
+                      className='btn btn-block'
+                      onClick={deliverHandler}
+                    >
+                      Mark As Delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
-}
+  );
+};
 
-export default OrderScreen
+export default OrderScreen;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer, productDeleteReducer, productCreateReducer, productUpdateReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer,userUpdateReducer } from './reducers/userReducers'
-import { orderCreateReducer, orderDetailsReducer, orderListReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailsReducer, orderListReducer, orderDeliverReducer } from './reducers/orderReducers'
 
 
 
@@ -25,6 +25,7 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderList: orderListReducer,
+    orderDeliver: orderDeliverReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
